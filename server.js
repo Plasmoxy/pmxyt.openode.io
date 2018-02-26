15abb99ddaf4d30704251e1677830396ddfce4c6
Plasmoxy <sebohacker@gmail.com>
2018-02-26 23:41:20 +0100

    acme and ssl

--- server.js
+++ server.js
@@ -1,18 +1,46 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var express = require('express');
+var http = require('http');
+var https = require('https');
+var fs = require('fs');
+var socketio = require('socket.io');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+var ssl_options = {
+  key: fs.readFileSync('ssl/private.key'),
+  cert: fs.readFileSync('ssl/certificate.crt'),
+  ca: fs.readFileSync('ssl/ca_bundle.crt')
+};
+
+app = express();
+redirectApp = express();
+
+// redirect from http
+var redirectServer = http.createServer(redirectApp);
+var server = https.createServer(ssl_options, app);
+var io = socketio.listen(server);
+
+// redirect all http requests to https
+redirectApp.get('*', function (req, res, next) {
+  !req.secure ? res.redirect('https://pmxyt.fr.openode.io' + req.url) : next();
+})
+
+// routing
+app.use('/.well-known', express.static(__dirname + '/.well-known', {dotfiles:'allow'}))
+
+app.use('/game', express.static(__dirname + '/game'))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/game/game.html')
+})
+
+io.sockets.on('connection', function(client) {
+  client.on('alpha', function(msg) {
+    //client.emit('alpha', msg);
+  })
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
-
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+server.listen(443, function() {
+  console.log('https server listening on port 443')
+})
+redirectServer.listen(80, function() {
+  console.log('redirect http server listening on port 80')
+})
